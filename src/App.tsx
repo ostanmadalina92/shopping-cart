776c06fd11ed4ebb94f1e9947618e58b745a1b03
ostanmadalina92 <ostan.madalina@yahoo.com>
2023-01-05 17:30:28 +0200

    style items

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
-import Drawer from '@mui/material/Drawer';
-import LinearProgress from '@mui/material/LinearProgress';
-import Grid from '@mui/material/Grid';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import Badge from '@mui/material/Badge';
+import Drawer from "@mui/material/Drawer";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Badge from "@mui/material/Badge";
 
 import { Wrapper } from "./App.styles";
+import Item from "./Item";
 
 export type CartItemType = {
   id: number;
@@ -17,22 +18,36 @@ export type CartItemType = {
   price: number;
   title: string;
   amount: number;
-}
+};
 
-const getProducts =  async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 function App() {
-  const  { data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
-  console.log(data)
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
 
   const getTotalItems = () => null;
   const handleAddToCart = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
 
-  if (isLoading) return <LinearProgress/>;
+  if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong ...</div>;
 
-  return <div className="App">start</div>;
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 }
 
 export default App;
--- src/Item.tsx
+++ src/Item.tsx
@@ -11,7 +11,12 @@ const Item: React.FC<Props> = ({item, handleAddToCart}) => (
     <Wrapper>
         <img src={item.image} alt={item.title}/>
         <div>
-            
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
         </div>
+        <Button onClick={() => handleAddToCart(item)} >Add to cart</Button>
     </Wrapper>
 )
+
+export default Item;
